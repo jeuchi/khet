ec67786dac03dd762a33c57f42428f06499fa7ee
Jeremy Euchi <jeuchi@icloud.com>
2024-11-17 13:36:49 -0800

    Update path

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    base: "/khet",
+    base: "/khet/ui",
     plugins: [react()],
     server: {
       hmr: {
