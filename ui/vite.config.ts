0212946d405807c0f1189ad14aa58428ad7c19cc
Jeremy Euchi <jeuchi@icloud.com>
2024-11-17 14:04:48 -0800

    Fix Netlify

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    base: "/khet",
     plugins: [react()],
     server: {
       hmr: {
